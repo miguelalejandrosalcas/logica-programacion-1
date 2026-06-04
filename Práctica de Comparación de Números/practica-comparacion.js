943dece776683033796f4417f044364bb941e5d4
Miguel Alejandro <miguelalejandrosalcas@gmail.com>
2026-06-04 00:31:30 -0500

    Corrección y finalización práctica laboratorio

--- Práctica de Comparación de Números/practica-comparacion.js
+++ Práctica de Comparación de Números/practica-comparacion.js
@@ -17,9 +17,15 @@ if (numero1 === numero2 && numero2 === numero3) {
 } else if (numero2 >= numero1 && numero1 >= numero3){
     formaAsc.textContent = `De mayor a menor ${numero2}, ${numero1}, ${numero3}`;
     formaDesc.textContent = `De menor a mayor ${numero3}, ${numero1}, ${numero2}`;
-} else {
+} else if (numero2 >= numero3 && numero3 >= numero1){
     formaAsc.textContent = `De mayor a menor ${numero2}, ${numero3}, ${numero1}`;
     formaDesc.textContent = `De menor a mayor ${numero1}, ${numero3}, ${numero2}`;
+} else if (numero3 >= numero1 && numero1 >= numero2){
+    formaAsc.textContent = `De mayor a menor ${numero3}, ${numero1}, ${numero2}`;
+    formaDesc.textContent = `De menor a mayor ${numero2}, ${numero1}, ${numero3}`;
+} else if (numero1 >= numero3 && numero3 >= numero2){
+    formaAsc.textContent = `De mayor a menor ${numero1}, ${numero3}, ${numero2}`;
+    formaDesc.textContent = `De menor a mayor ${numero2}, ${numero3}, ${numero1}`;
 }
 
 
